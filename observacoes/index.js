f4ada4906d2d478a13e6b25321272f6c0de1aaa9
Guilherme Mendes <guilhermemendeds@gamil.com>
2021-11-07 14:31:22 -0300

    Insercao dos endpoints em observacoes

---
+++ observacoes/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const { v4: uuidv4 } = require('uuid')
+
+const app = express()
+app.use(bodyParser.json())
+
+const observacoesPorLembreteId = {}
+
+app.get('/lembretes/:id/observacoes', (req, res) => {
+    res.send(observacoesPorLembreteId[req.params.id] || [])
+})
+
+app.post(`/lembretes/:id/observacoes`, (req, res) => {
+    const idObs = uuidv4()
+    const { texto } = req.body
+    const idLembrete = req.params.id
+    const observacoesDoLembrete = observacoesPorLembreteId[idLembrete] || []
+    observacoesDoLembrete.push({ id: idObs, texto })
+    observacoesPorLembreteId[idLembrete] = observacoesDoLembrete
+    res.status(201).send(observacoesDoLembrete)
+})
+
+app.listen(3000, () => console.log("3000 post available for observartions"))
