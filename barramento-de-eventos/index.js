70d729db94a77530dfc89dd6283094b52732457d
Guilherme Mendes <guilhermemendeds@gamil.com>
2021-11-07 18:43:37 -0300

    Inicio da implementacao do barramento de eventos

---
+++ barramento-de-eventos/index.js
@@ -0,0 +1,18 @@
+const express = require('express') //Serve para receber requisições HTTP
+const bodyParser = require('body-parser')
+const axios = require('axios') //Serve para fazer requisições HTTP
+
+const app = express()
+app.use(bodyParser.json()) //Com isso eu consigo tratar o corpo de uma requisição como JSON, se ele for JSON
+
+app.post('/eventos', (req, res) => {
+    const evento = req.body
+    //Envia o evento para o microsserviço de lembretes
+    axios.post('http://localhost:4000/eventos', evento)
+    //Envia o evento para o microsserviço de observações
+    axios.post('http://localhost:5000/eventos', evento)
+    //Finalizo para não estourar por só fazer fazer a "requisição" e não "responder" nada
+    res.status(204).end()
+})
+
+app.listen(10000, () => console.log("Barramento de eventos. Porta 10000."))
--- observacoes/index.js
+++ observacoes/index.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const { v4: uuidv4 } = require('uuid')
+const { v4: uuidv4 } = require('uuid') //Pacote que gera IDs fortemente
 
 const app = express()
 app.use(bodyParser.json())
 
 const observacoesPorLembreteId = {}
 
+//EX: localhost:3000/lembretes/4/observacoes
 app.get('/lembretes/:id/observacoes', (req, res) => {
+    //Trazemos atraves do GET o OBJ com o mesmo ID passados nos parametros da URL ou se não existir nos traz um array vazio
     res.send(observacoesPorLembreteId[req.params.id] || [])
 })
 
@@ -15,9 +17,13 @@ app.post(`/lembretes/:id/observacoes`, (req, res) => {
     const idObs = uuidv4()
     const { texto } = req.body
     const idLembrete = req.params.id
+    //Criamos um ARRAY que recebe o OBJ com o ID da REQUISICAO ou uma lista vazia caso não tenham passado o parametro do ID na requisição
     const observacoesDoLembrete = observacoesPorLembreteId[idLembrete] || []
+    //Damos um PUSH no ARRAY criado acima, passando seus elementos, sendo que "texto" por ter exatamente o mesmo nome CHAVE/VALOR, é possivel ser passado somente uma vez para facilitar o "texto: texto"
     observacoesDoLembrete.push({ id: idObs, texto })
+    //Atribuimos ao OBJETO com o ID da REQUISICAO o ARRAY criado acima
     observacoesPorLembreteId[idLembrete] = observacoesDoLembrete
+    //Exibimos o ARRAY criado
     res.status(201).send(observacoesDoLembrete)
 })
 
